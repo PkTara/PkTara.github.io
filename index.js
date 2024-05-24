4fb617b275f54e9f055b36c1a5032c65b168293f
PkTara <tarfahey@gmail.com>
2024-05-24 22:47:54 +0000

    added willpower functionality

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var dice = [0,0] // 0 is hunger, 1 is normal
 var atrPoints = []
 
 function randi(max) {
     return Math.floor(Math.random() * max);
   }
 
-function calcDiceType() {
+function getDicePools() { // Into normDice, hungerDice
     var totalDice = getTotalDice_fromAttributes()
     var hunger = parseInt(document.getElementById("hunger").value)
 
@@ -17,72 +16,71 @@ function calcDiceType() {
     return([normDice, hungerDice])
 }
 
-function noDice() { // index - 0 is hunger, 1 is normal
-    
-   // dice[index] = number
-  //  var normDice = (dice[1] - dice[0] > 0) ? (dice[1] - dice[0]) : 0;
-   // var hungerDice = Math.min(dice[0], dice[1])
+function printPotentialDice(clear=true) {
+    var [normDice, hungerDice] = getDicePools() 
 
-    var [normDice, hungerDice] = calcDiceType() 
+    if (clear) {clearDiceBox()}
 
-    clearDiceBox()
-
-    for (let type of [[normDice, 1], [hungerDice, 0]]) {
-        for (let i=0; i < type[0]; i++) {
-            document.getElementById(['hungerDiceBox','normalDiceBox'][type[1]])
-            .innerHTML += `<img src='img/rolls/${ ["red-fail.png", "normal-fail.png"][type[1]]}'>`
+    for (const [index, dicePool] of [hungerDice, normDice].entries()){
+        for (let i=0; i < dicePool; i++) {
+            document.getElementById(['hungerDiceBox','normalDiceBox'][index])
+            .innerHTML += `<img src='img/rolls/${ ["red-fail.png", "normal-fail.png"][index]}'>`
             }
         }
 }
 
-function clearDiceBox() {
-    for (let box of ["hungerDiceBox" , "normalDiceBox"]) {
-        document.getElementById(box).innerHTML = ""
+function clearDiceBox(clearHunger=true, clearNormal=true) {
+    for (let [index, box] of ["hungerDiceBox" , "normalDiceBox"].entries()) {
+        if ([clearHunger, clearNormal][index]) {console.log(box);document.getElementById(box).innerHTML = ""}
+        console.log(clearHunger)
     }
+
 }
-function printDice(normalDice, hungerDice) {
-    clearDiceBox()
+function printDice(normalDice, hungerDice, clear=true) {
+    if (clear) {clearDiceBox()}
 
-    var [normalDice, normalSuccesses] = numberToRoll(normalDice, 0)
-    var [hungerDice, hungerSuccesses] = numberToRoll(hungerDice, 1)
+    var [normalDice, normalSuccesses] = evaluateDicePool(normalDice, 0)
+    var [hungerDice, hungerSuccesses] = evaluateDicePool(hungerDice, 1)
 
-    for (let type of [[normalDice, 1], [hungerDice, 0]]) {
-        for (let img of type[0]) {
-            document.getElementById(['hungerDiceBox','normalDiceBox'][type[1]])
-            .innerHTML += `<img src='${img}'>`
+    for (const [index, dicePoolIMG] of [hungerDice, normalDice].entries()) {
+        for (let img of dicePoolIMG) {
+            document.getElementById(['hungerDiceBox','normalDiceBox'][index])
+            .innerHTML += `<img class=${["hungerIMG", "normalIMG"][index]} src='${img}'>`
             }
         }
 
-    printSuccesses(normalSuccesses, hungerSuccesses)
+    printResultText(normalSuccesses, hungerSuccesses)
 }
 
-function printSuccesses(normalSuccesses, hungerSuccesses) { // success, crit, bestial
-    var totalSuccesses = normalSuccesses[0] + hungerSuccesses[0] + normalSuccesses[1] + hungerSuccesses[1]
-    var totalPairs = (Math.floor((normalSuccesses[1] + hungerSuccesses[1]) / 2)) * 2
-    totalSuccesses += (Math.floor((normalSuccesses[1] + hungerSuccesses[1]) / 2)) * 2
+function printResultText(normalSuccesses, hungerSuccesses) { // [success, crit, bestial]
+    console.log(normalSuccesses["crit"])
+    var totalSuccesses = normalSuccesses["success"] + hungerSuccesses["success"] + normalSuccesses["crit"] + hungerSuccesses["crit"]
+    var totalPairs = (Math.floor((normalSuccesses["crit"] + hungerSuccesses["crit"]) / 2)) * 2
+    totalSuccesses += (Math.floor((normalSuccesses["crit"] + hungerSuccesses["crit"]) / 2)) * 2
    
     var difficulty = parseInt(document.getElementById("diffOpt").value )
-    console.log(difficulty)
+    var notFail = totalSuccesses >= difficulty // Did roll succeed? (but that word is used way too much here...)
 
     var addToHTML = ``
     addToHTML += 
     `<h4>Successes: ${totalSuccesses}</h4>`
-    if (hungerSuccesses[2]) {
-        addToHTML += " <p> Possible bestial failure</p>"
+    if (hungerSuccesses["bestial"] && (difficulty == 0 || !notFail)) {
+        addToHTML += `<p> ${difficulty ? "" : "Possible"} Bestial Failure</p>`
     }
-    if (totalPairs && hungerSuccesses[1]) {
-        addToHTML += "<p>Possible Messy Critical</p>"
+    if (totalPairs && hungerSuccesses["crit"] && (difficulty == 0 || notFail)) {
+        addToHTML += `<p>${difficulty ? "" : "Possible"} Messy Critical</p>`
+    }
+    if (difficulty) { 
+        addToHTML += `<p><strong>${notFail ? "Roll Succeeded!" : "Roll Failed"}</strong></p>`
     }
 
 
     document.getElementById("result").innerHTML = addToHTML
 }
 
-function willpower() {
-    document.getElementById()
-}
-function rollDice () {
-    var [no_normal, no_hunger] = calcDiceType()
+
+function rollDice (noDice) {
+    var [no_normal, no_hunger] = noDice ? [noDice, 0] : getDicePools()  // if noDice was passed as parameter, use it, else find out from user options
 
 
     var normalDice = []
@@ -92,26 +90,28 @@ function rollDice () {
             dicePool[1].push(randi(10) + 1)
         }
     }
-    printDice(normalDice, hungerDice)
+
+    printDice(normalDice, hungerDice, noDice ? false : true)
+    
 }
-function numberToRoll(diceList, isHunger) {
-    var successes = [0,0,0] // normal, critical, bestial
+function evaluateDicePool(diceList, isHunger) {
+    var successes = {"success" : 0, "crit" : 0, "bestial" : 0, "isHunger": isHunger}
     var diceIMGlist = []
     for (let dice of diceList) {
         if (dice==1 && isHunger) {
             diceIMGlist.push("img/rolls/bestial-fail.png")
-            successes[2] += 1
+            successes["bestial"] += 1
         }
         else if (dice < 6) {
             diceIMGlist.push(`img/rolls/${["normal", "red"][isHunger]}-fail.png`)
         }
         else if (dice==10) {
             diceIMGlist.push(`img/rolls/${["normal", "red"][isHunger]}-crit.png`)
-            successes[1] += 1
+            successes["crit"] += 1
         }
         else if (dice < 10) {
             diceIMGlist.push(`img/rolls/${["normal", "red"][isHunger]}-success.png`)
-            successes[0] += 1
+            successes["success"] += 1
         }
         else {
             console.log("ERROR:", dice)
@@ -120,6 +120,59 @@ function numberToRoll(diceList, isHunger) {
 
     return([diceIMGlist, successes])
     }
+
+function willpower() {
+    console.log("I GOT THE POWER!")
+    var normalDice = Object.values(document.getElementsByClassName("normalIMG"))
+    if (normalDice.length) { // If there are any normal dice,
+        var addToHTML = `<div id="willpower-dice">`
+        for (var ankh of normalDice) {
+            addToHTML += `<btn class="will" onclick=selectDie(this)><img src=${ankh.src}></btn>`
+        }
+        addToHTML += `</div>`
+        document.getElementById("normalDiceBox").innerHTML = addToHTML
+    
+        document.getElementById("willpower").innerHTML = "Reroll"
+        document.getElementById("willpower").onclick = willpowerReroll
+
+}
+}
+function selectDie(button) {
+    if (Object.values(button.classList).includes("will-selected")) {
+        button.classList.remove("will-selected")
+    } 
+    else if (document.getElementsByClassName("will-selected").length < 3){
+        button.classList.add("will-selected")
+    }
+    }
+function willpowerReroll() {
+    var selected = document.getElementsByClassName("will-selected")
+    rollDice(selected.length)
+    while(selected.length > 0){
+        selected[0].parentNode.removeChild(selected[0]);
+        }
+    
+
+    var addToHTML = ""
+    var willDice = Object.values(document.getElementsByClassName("will"))
+    console.log(willDice)
+    if (willDice.length) { // If there are any will dice left,
+        for (var ankh of willDice) {
+            addToHTML += `<img class="normalIMG" src=${ankh.children[0].src}>`
+        }}
+        document.getElementById("normalDiceBox").innerHTML += addToHTML
+    
+    
+    willDice = document.getElementsByClassName("will")
+    while(willDice.length > 0){
+        willDice[0].parentNode.removeChild(willDice[0]);
+        }
+
+       
+    document.getElementById("willpower").innerHTML = "Willpower"
+    document.getElementById("willpower").onclick = willpower
+}
+
 
 
 function rouseCheck() {
